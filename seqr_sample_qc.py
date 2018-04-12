1ea2e87f198cf2345bd4fbd5f79cfb26ebdf7dfa
gtiao <gtiao@broadinstitute.org>
2018-04-12 16:13:03 -0400

    starting seqr sample qc pipeline at last

---
+++ seqr_sample_qc.py
@@ -0,0 +1,20 @@
+from gnomad_hail import *
+from gnomad_hail.resources.sample_qc import *
+
+logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
+logger = logging.getLogger("seqr_sample_qc")
+logger.setLevel(logging.INFO)
+
+
+def main(args):
+    hl.init()
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--my-first-argument')
+    args = parser.parse_args()
+
+    main(args)
