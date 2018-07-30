0c40fc5d351e8c64029b8b5130d35b7308c6ecf8
Mike Wilson <mwilson@broadinstitute.org>
2018-07-30 10:40:48 -0400

    Addressed Grace's Comments

--- resources/resources_seqr_qc.py
+++ resources/resources_seqr_qc.py
@@ -1,105 +1,101 @@
-def callset_vcf_path(data_type: str, external: bool, version: int, test: bool) -> str:
+def get_filepath(data_type: str, is_external: bool, version: int, test: bool) -> str:
+    """
+    Returns string with file path to callset version
+    :param data_type: String of WES or WGS
+    :param is_external: boolean whether data is external or not
+    :param version: int version of callset
+    :param test: Used if running pipeline on designated test data
+    :return:
+    """
+    origin = "External" if is_external else "Internal"
+    build = "GRCh38" if data_type == "WGS" and not is_external else "GRCh37"
+    if test:
+        return f'gs://seqr-datasets/methods_dev/test_data/GRCh37/topf'  # TODO rename test vcf
+    else:
+        return f'gs://seqr-datasets/{build}/RDG_{data_type}_Broad_{origin}/{version}/RDG_{data_type}_Broad_{origin}_{version}'
+
+
+def callset_vcf_path(data_type: str, is_external: bool, version: int, test: bool) -> str:
     """
     Returns callset vcf path. Can be internal or external, exomes or genomes.
     """
-    #Need to work version in because if overwrite - need to reload every project everytime so return .../v' + version + "/callset_Vcf"
-    #Need callsets to be named the same everytime...should it just grab whatever vcf is in the directory? or fix naming with mv in bucket?
-    if test:
-        return f'gs://seqr-datasets/methods_dev/test_data/GRCh37/CMG_MYOSEQ.vcf.gz'
-    if data_type not in ('exomes', 'genomes'):
-        raise DataException("Select data_type as either 'exomes' or genomes'")
-    if data_type == 'exomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External.vcf.gz'
-    if data_type == 'exomes' and not external:
-        return f'gs://seqr-datasets/GRCh37/CMG_MYOSEQ/v{version}/CMG_MYOSEQ.vcf.gz'
-    if data_type == 'genomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External.vcf.gz'
-    if data_type == 'genomes' and not external:
-        return f'gs://seqr-datasets/GRCh38/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets/v{version}/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets_v{version}.vcf.gz'
+    filepath = get_filepath(data_type, is_external, version, test)
+    return filepath + '.vcf.gz'
 
 
-def mt_path(data_type: str, external: bool, version: int, test: bool) -> str:
+def mt_path(data_type: str, is_external: bool, version: int, test: bool) -> str:
     """
     Returns MatrixTable for seqr sample qc purposes: can be exomes or genomes, internal or external data.
     Generated from vcf.
     """
-    if test:
-        return f'gs://seqr-datasets/methods_dev/test_data/GRCh37/topf_test_seqr_qc.mt'
-    if data_type not in ('exomes', 'genomes'):
-        raise DataException("Select data_type as either 'exomes' or genomes'")
-    if data_type == 'exomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External.mt'
-    if data_type == 'exomes' and not external:
-        return f'gs://seqr-datasets/GRCh37/CMG_MYOSEQ/v{version}/CMG_MYOSEQ.mt'
-    if data_type == 'genomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External.mt'
-    if data_type == 'genomes' and not external:
-        return f'gs://seqr-datasets/GRCh38/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets/v{version}/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callset.mt'
+    filepath = get_filepath(data_type, is_external, version, test)
+    return filepath + '.mt'
 
 
-def qc_mt_path(data_type: str, external: bool, version: int, test: bool) -> str:
+def qc_mt_path(data_type: str, is_external: bool, version: int, test: bool) -> str:
     """
     Returns seqr sample qc MatrixTable: can be exomes or genomes, internal or external data.
     Contains vcf sample ID, seqr sample ID, callrate, chimera, contamination, coverage, pedigree information,
     provided gender and affected status, computed gender, platform, ancestry, inbreeding coefficient.
     """
-    if test:
-        return f'gs://seqr-datasets/methods_dev/test_data/GRCh37/topf_test_seqr_qc.mt'
-    if data_type not in ('exomes', 'genomes'):
-        raise DataException("Select data_type as either 'exomes' or genomes'")
-    if data_type == 'exomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External_seqr_qc.mt'
-    if data_type == 'exomes' and not external:
-        return f'gs://seqr-datasets/GRCh37/CMG_MYOSEQ/v{version}/CMG_MYOSEQ_seqr_qc.mt'
-    if data_type == 'genomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External_seqr_qc.mt'
-    if data_type == 'genomes' and not external:
-        return f'gs://seqr-datasets/GRCh38/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets/v{version}/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets_v{version}_seqr_qc.mt'
+    filepath = get_filepath(data_type, is_external, version, test)
+    return filepath + '_seqr_qc.mt'
 
 
-def metadata_path(data_type: str, external: bool, version: int, test: bool) -> str:
+def metadata_path(data_type: str, is_external: bool, version: int, test: bool) -> str:
     """
     Path to metadata file associated with samples in the callset for seqr sample QC
     """
-    if test:
-        return f'gs://seqr-datasets/methods_dev/test_data/GRCh37/topf_meta.txt'
-    if data_type not in ('exomes', 'genomes'):
-        raise DataException("Select data_type as either 'exomes' or genomes'")
-    if data_type == 'exomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External_metadata.txt'
-    if data_type == 'exomes' and not external:
-        return f'gs://seqr-datasets/GRCh37/CMG_MYOSEQ/v{version}/CMG_MYOSEQ_metadata.txt'
-    if data_type == 'genomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External_metadata.txt'
-    if data_type == 'genomes' and not external:
-        return f'gs://seqr-datasets/GRCh38/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets/v{version}/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets_v{version}_metadata.txt'
+    filepath = get_filepath(data_type, is_external, version, test)
+    return filepath + '_metadata.txt'
 
 
-def remap_path(data_type: str, external: bool, test: bool) -> str:
+def remap_path(data_type: str, is_external: bool, version: int, test: bool) -> str:
     """
     Path to remap ID file
     """
-    if test:
-        return f'gs://seqr-datasets/methods_dev/test_data/GRCh37/topf_remap.txt'
-    if data_type not in ('exomes', 'genomes'):
-        raise DataException("Select data_type as either 'exomes' or genomes'")
-    if data_type == 'exomes' and external:
-        return f'gs://seqr-datasets/callset_id_remap_files/MacArthur_WGS_Broad_External_remap.txt'
-    if data_type == 'exomes' and not external:
-        return f'gs://seqr-datasets/callset_id_remap_files/CMG_MYOSEQ_remap.txt'
-    if data_type == 'genomes' and external:
-        return f'gs://seqr-datasets/callset_id_remap_files/MacArthur_WGS_Broad_External_remap.txt'
-    if data_type == 'genomes' and not external:
-        return f'gs://seqr-datasets/callset_id_remap_files/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets_remap.txt'
+    filepath = get_filepath(data_type, is_external, version, test)
+    return filepath + '_remap.txt'
+
+
+def sex_check_path(data_type: str, is_external: bool, version: int, test: bool) -> str:
+    """Return file path for sex check"""
+    filepath = get_filepath(data_type, is_external, version, test)
+    return filepath + '.sex_check.txt.bgz'
 
 
 def sex_check_name():
     return "test"
 
 
-# Using full ped right now? Do we need sex check concordance right away or can it happen after subset?
 def ped_path():
-    return "gs://seqr-dbgap/cmg_topf_cms_wes.ped"
+    return "gs://seqr-dbgap/cmg_topf_cms_wes.ped"  # TODO Use Ben's API to pull ped of all individuals in callset from seqr
+
+
+class DataException(Exception):
+    pass
+
+
+def exome_callrate_mt_path(is_external: bool, version: int, test: bool) -> str:
+    data_type = "WES"
+    filepath = get_filepath(data_type, is_external, version, test)
+    return filepath + '_callrate_mt_for_pca.mt'
+
+
+def exome_callrate_scores_ht_path(is_external: bool, version: int, test: bool) -> str:
+    data_type = "WES"
+    filepath = get_filepath(data_type, is_external, version, test)
+    return filepath + '_callrate_pca_scores.ht'
+
+
+def qc_temp_data(data_type: str, is_external: bool, test: bool, version: int) -> str:
+    filepath = get_filepath(data_type, is_external, version, test)
+    return filepath + '_platform_pca/'
+
+
+def platform_labels_path(data_type: str, is_external: bool, test: bool, version: int) -> str:
+    filepath = get_filepath(data_type, is_external, version, test)
+    return filepath + 'platforms.txt'
 
 
 class DataException(Exception):
--- seqr_sample_qc.py
+++ seqr_sample_qc.py
@@ -1,147 +1,30 @@
 from gnomad_hail import *
-#from resources.resources_seqr_qc import *
+from resources.resources_seqr_qc import *
 import hail as hl
 import logging
 import sys
 import argparse
 
-"""
-import hail.expr.aggregators as agg
-import numpy as np
-import re
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-from collections import Counter, defaultdict
-from os.path import basename, splitext
-from math import log, isnan
-from pprint import pprint
-"""
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 logger = logging.getLogger("seqr_sample_qc")
 logger.setLevel(logging.INFO)
 
-def callset_vcf_path(data_type: str, external: bool, version: int, test: bool) -> str:
-    """
-    Returns callset vcf path. Can be internal or external, exomes or genomes.
-    """
-    #Need to work version in because if overwrite - need to reload every project everytime so return .../v' + version + "/callset_Vcf"
-    #Need callsets to be named the same everytime...should it just grab whatever vcf is in the directory? or fix naming with mv in bucket?
-    if test:
-        return f'gs://seqr-datasets/methods_dev/test_data/GRCh37/CMG_MYOSEQ.vcf.gz'
-    if data_type not in ('exomes', 'genomes'):
-        raise DataException("Select data_type as either 'exomes' or genomes'")
-    if data_type == 'exomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External.vcf.gz'
-    if data_type == 'exomes' and not external:
-        return f'gs://seqr-datasets/GRCh37/CMG_MYOSEQ/v{version}/CMG_MYOSEQ.vcf.gz'
-    if data_type == 'genomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External.vcf.gz'
-    if data_type == 'genomes' and not external:
-        return f'gs://seqr-datasets/GRCh38/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets/v{version}/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets_v{version}.vcf.gz'
-
-
-def mt_path(data_type: str, external: bool, version: int, test: bool) -> str:
-    """
-    Returns MatrixTable for seqr sample qc purposes: can be exomes or genomes, internal or external data.
-    Generated from vcf.
-    """
-    if test:
-        return f'gs://seqr-datasets/methods_dev/test_data/GRCh37/topf_test_seqr_qc.mt'
-    if data_type not in ('exomes', 'genomes'):
-        raise DataException("Select data_type as either 'exomes' or genomes'")
-    if data_type == 'exomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External.mt'
-    if data_type == 'exomes' and not external:
-        return f'gs://seqr-datasets/GRCh37/CMG_MYOSEQ/v{version}/CMG_MYOSEQ.mt'
-    if data_type == 'genomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External.mt'
-    if data_type == 'genomes' and not external:
-        return f'gs://seqr-datasets/GRCh38/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets/v{version}/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callset.mt'
-
-
-def qc_mt_path(data_type: str, external: bool, version: int, test: bool) -> str:
-    """
-    Returns seqr sample qc MatrixTable: can be exomes or genomes, internal or external data.
-    Contains vcf sample ID, seqr sample ID, callrate, chimera, contamination, coverage, pedigree information,
-    provided gender and affected status, computed gender, platform, ancestry, inbreeding coefficient.
-    """
-    if test:
-        return f'gs://seqr-datasets/methods_dev/test_data/GRCh37/topf_test_seqr_qc.mt'
-    if data_type not in ('exomes', 'genomes'):
-        raise DataException("Select data_type as either 'exomes' or genomes'")
-    if data_type == 'exomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External_seqr_qc.mt'
-    if data_type == 'exomes' and not external:
-        return f'gs://seqr-datasets/GRCh37/CMG_MYOSEQ/v{version}/CMG_MYOSEQ_seqr_qc.mt'
-    if data_type == 'genomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External_seqr_qc.mt'
-    if data_type == 'genomes' and not external:
-        return f'gs://seqr-datasets/GRCh38/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets/v{version}/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets_v{version}_seqr_qc.mt'
-
-
-def metadata_path(data_type: str, external: bool, version: int, test: bool) -> str:
-    """
-    Path to metadata file associated with samples in the callset for seqr sample QC
-    """
-    if test:
-        return f'gs://seqr-datasets/methods_dev/test_data/GRCh37/topf_meta.txt'
-    if data_type not in ('exomes', 'genomes'):
-        raise DataException("Select data_type as either 'exomes' or genomes'")
-    if data_type == 'exomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External_metadata.txt'
-    if data_type == 'exomes' and not external:
-        return f'gs://seqr-datasets/GRCh37/CMG_MYOSEQ/v{version}/CMG_MYOSEQ_metadata.txt'
-    if data_type == 'genomes' and external:
-        return f'gs://seqr-datasets/GRCh37/MacArthur_WGS_Broad_External/v{version}/MacArthur_WGS_Broad_External_metadata.txt'
-    if data_type == 'genomes' and not external:
-        return f'gs://seqr-datasets/GRCh38/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets/v{version}/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets_v{version}_metadata.txt'
-
-
-def remap_path(data_type: str, external: bool, test: bool) -> str:
-    """
-    Path to remap ID file
-    """
-    if test:
-        return f'gs://seqr-datasets/methods_dev/test_data/GRCh37/topf_remap.txt'
-    if data_type not in ('exomes', 'genomes'):
-        raise DataException("Select data_type as either 'exomes' or genomes'")
-    if data_type == 'exomes' and external:
-        return f'gs://seqr-datasets/callset_id_remap_files/MacArthur_WGS_Broad_External_remap.txt'
-    if data_type == 'exomes' and not external:
-        return f'gs://seqr-datasets/callset_id_remap_files/CMG_MYOSEQ_remap.txt'
-    if data_type == 'genomes' and external:
-        return f'gs://seqr-datasets/callset_id_remap_files/MacArthur_WGS_Broad_External_remap.txt'
-    if data_type == 'genomes' and not external:
-        return f'gs://seqr-datasets/callset_id_remap_files/CMG_RGP_Broad_MacArthur_RareDisease_WGS_Callsets_remap.txt'
-
-def sex_check_name():
-    return "test"
-
-
-# Using full ped right now? Do we need sex check concordance right away or can it happen after subset?
-def ped_path():
-    return "gs://seqr-dbgap/cmg_topf_cms_wes.ped"
-
-
-class DataException(Exception):
-    pass
-
 
 def apply_filter_flags_expr(ht: hl.Table, data_type: str) -> hl.expr.SetExpression:
     """
+    Annotates table with flags for elevated contamination and chimera as well as low coverage and call rate
     :param: Table ht: input MT
-    :param: str data_type: 'exomes' or 'genomes'
+    :param: str data_type: 'WES' or 'WGS'
     :return: output MT
     :rtype: SetExpression
     """
     filters = {
         'callrate_less_than_0.85': ht.callrate < 0.85,
-        'contamination_greater_than_0.05': ht.contamination > 0.05,
+        'contamination_greater_than_0.05': ht.contamination > 0.05,  # TODO determine thresholds for contamination and chimera
         'chimera_greater_than_0.05': ht.chimera > 0.05
     }
-    if data_type == 'exomes':
+    if data_type == 'WES':
         filters.update({
             'coverage_less_than_85_at_20x': ht.bases_at_20x < 0.85
         })
@@ -154,30 +37,27 @@ def apply_filter_flags_expr(ht: hl.Table, data_type: str) -> hl.expr.SetExpressi
                             [hl.or_missing(filter_expr, name) for name, filter_expr in filters.items()]))
 
 
-def compute_sex(mt: hl.MatrixTable, out_internal_mt_prefix: str, test: bool,
-                male_threshold: float = 0.8, female_threshold: float = 0.5) -> hl.MatrixTable:
+def compute_sex(mt: hl.MatrixTable, data_type: str, is_external: bool, version: int, test: bool,
+                male_threshold: float, female_threshold: float) -> hl.MatrixTable:
     """
-    Imputes sex, exports data, and annotates mt with this data
-    NOTE: Evaluated in R (plots) and decided on cutoff of F<0.5 for females and F>0.8 for males (default) for genomes
+    Imputes sex, exports data, and annotates mt with this data # TODO evaluate F cut offs for females and males - run internal and external
     :param MatrixTable mt: MT containing samples to be ascertained for sex
-    :param str out_internal_mt_prefix: file path prefix for tsv containing samples and sex imputation annotations
+    :param string data_type: WES or WGS
+    :param boolean is_external: Whether data is external
+    :param integer version: Callset version number
     :param string male_threshold: determined from r plots
     :param string female_threshold: determined from r plots
     :param boolean test: boolean for test data
     :return: MatrixTable with imputed sex annotations stashed in column annotation 'sex_check'
     :rtype: MatrixTable
     """
-    # need to preplot this...for whole callset to determine thresholds? just project?
-    # complain on X GRCH 38
-    # will dataset be split prior to this?
-    # SNV AND BIALLELIC FILTER?
 
     # filter to the X chromosome
     mt = hl.filter_intervals(mt, [hl.parse_locus_interval('X')])
 
     # create a sex_ht using hail’s impute sex function and export the resulting table
     sex_ht = hl.impute_sex(mt.GT, aaf_threshold=0.05, female_threshold=female_threshold, male_threshold=male_threshold)
-    sex_ht.export(out_internal_mt_prefix + '.sex_check.txt.bgz')
+    sex_ht.export(sex_check_path(data_type, is_external, version, test))
 
     # select three columns from the sex_ht and key by sample
     sex_colnames = ['f_stat', 'is_female']
@@ -185,7 +65,8 @@ def compute_sex(mt: hl.MatrixTable, out_internal_mt_prefix: str, test: bool,
     mt = mt.annotate_cols(**sex_ht[mt.col_key])
 
     if test:
-        y_coverage_ht = hl.import_table('gs://seqr-datasets/methods_dev/test_data/GRCh37/topf_y_coverage.txt', impute=True).key_by('sample_id')
+        y_coverage_ht = hl.import_table('gs://seqr-datasets/methods_dev/test_data/GRCh37/topf_y_coverage.txt',
+                                        impute=True).key_by('sample_id')
         mt = mt.annotate_cols(**y_coverage_ht[mt.col_key])
 
     return mt
@@ -214,35 +95,36 @@ def annotate_sex(mt: hl.MatrixTable):
 def main(args):
     hl.init()
 
-    data_type = "genomes" if args.genomes else "exomes"
+    data_type = "WGS" if args.genomes else "WES"
     build = args.build
-    external = True if args.external else False
+    is_external = True if args.is_external else False
     version = args.callset_version
     test = True if args.test else False
+    male_threshold = args.male_threshold
+    female_threshold = args.female_threshold
 
     if not args.skip_write_qc_mt:
-        vcf = (callset_vcf_path(data_type, build, external, test))
+        vcf = callset_vcf_path(data_type, build, is_external, test)
         hl.import_vcf(vcf, force_bgz=True, reference_genome=f'GRCh{build}',
-                      min_partitions=4).write(mt_path(data_type, external, version, test),
-                                              overwrite=True)
-        mt = hl.read_matrix_table(mt_path(data_type, external, version, test))
+                      min_partitions=4).write(mt_path(data_type, is_external, version, test), args.overwrite)
+        mt = hl.read_matrix_table(mt_path(data_type, is_external, version, test))
         logger.info("Filtering to bi-allelic, high-callrate, common SNPs for sample QC...")
         mt = mt.filter_rows((hl.len(mt.alleles) == 2) & hl.is_snp(mt.alleles[0], mt.alleles[1])
                             & (hl.agg.mean(mt.GT.n_alt_alleles()) / 2 > 0.001) &
                             (hl.agg.fraction(hl.is_defined(mt.GT)) > 0.99))
         mt.annotate_cols(callrate=hl.agg.fraction(hl.is_defined(mt.GT))).naive_coalesce(5000).write(
-            qc_mt_path(data_type, external, version, test), overwrite=True)
+            qc_mt_path(data_type, is_external, version, test), args.overwrite)
 
-    qc_mt = hl.read_matrix_table(qc_mt_path(data_type, external, version, test))
+    qc_mt = hl.read_matrix_table(qc_mt_path(data_type, is_external, version, test))
 
     # optional flag for path that will override default logic
     logger.info("Importing and annotating with metadata...")
-    meta_ht = hl.import_table(metadata_path(data_type, external, version, test),
-                              impute=True).key_by('sample_id')
+    meta_ht = hl.import_table(metadata_path(data_type, is_external, version, test),
+                              impute=True).key_by('sample')
     qc_mt = qc_mt.annotate_cols(**meta_ht[qc_mt.s])
 
     logger.info("Importing and annotating seqr ID names...")
-    remap_ht = hl.import_table(remap_path(data_type, external, test),  # function remap_path
+    remap_ht = hl.import_table(remap_path(data_type, is_external, version, test),
                                impute=True).key_by('sample_id')
     qc_mt = qc_mt.annotate_cols(**remap_ht[qc_mt.s])
     remap_expr = hl.cond(hl.is_missing(qc_mt.seqr_id), qc_mt.s, qc_mt.seqr_id)
@@ -252,21 +134,21 @@ def main(args):
     qc_mt = qc_mt.annotate_cols(filter_flags=apply_filter_flags_expr(qc_mt, data_type))
 
     logger.info("Computing and annotating sex...")
-    qc_mt = compute_sex(qc_mt, "test")  # functionalize sex_check_name
-    qc_mt = annotate_sex(qc_mt)
+    qc_mt = compute_sex(qc_mt, data_type, is_external, version, test, male_threshold, female_threshold)
+    qc_mt = annotate_sex(qc_mt)  # TODO need to add args for variable thresholds
 
     logger.info("Annotating with pedigree concordance...")
-    ped = ped_path()  # functionalize ped_path
+    ped = ped_path()  # Will we make a massive ped file?
     ped_ht = hl.import_table(ped, impute=True).key_by('individual')
     qc_mt = qc_mt.annotate_cols(**ped_ht[qc_mt.s])
     gender_expr = hl.cond(qc_mt.gender == 1, "male", hl.cond(qc_mt.gender == 2, "female", "unknown"))
-    qc_mt = qc_mt.annotate_cols(new_gender=gender_expr)
-    concordance_expr = hl.cond(qc_mt.new_gender == qc_mt.sex, True,
+    qc_mt = qc_mt.annotate_cols(ped_sex=gender_expr)
+    concordance_expr = hl.cond(qc_mt.ped_sex == qc_mt.sex, True,
                                False)  # gender is what is given in pedigree and sex is what was imputed
     qc_mt = qc_mt.annotate_cols(sex_concordance=concordance_expr)
 
-    qc_mt.cols().export("partA_qc_mt.tsv")
-    qc_mt.cols().write(qc_mt_path(data_type, external, version, test))
+    qc_mt.export(get_filepath(data_type, is_external, version, test) + ".annotations.txt.bgz")
+    qc_mt.write(qc_mt_path(data_type, is_external, version, test))
 
 
 if __name__ == '__main__':
@@ -281,6 +163,7 @@ if __name__ == '__main__':
     parser.add_argument('--skip_write_qc_mt',
                         help='Skip writing pre-calculated MatrixTable containing high-quality variants',
                         action='store_true')
+
     parser.add_argument('-b', '--build', help='Reference build, 37 or 38', choices=["37", "38"], required=True)
     parser.add_argument('-o', '--output', help='Override default output directory with given directory.')
     parser.add_argument('--slack_channel', help='Slack channel to post results and notifications to.')
@@ -289,6 +172,9 @@ if __name__ == '__main__':
     parser.add_argument('--external', help='External sample data in callset', action='store_true')
     parser.add_argument('--test', help='To run a test of the pipeline using test files and directories',
                         action='store_true')
+    parser.add_argument('--overwrite', help='Overwrite previous QC for callset version', actions='store_true')
+    parser.add_argument('--male_threshold', help='Male f threshold for computing sex', type=float, default=0.8)
+    parser.add_argument('--female_threshold', help='Female f threshold for computing sex', type=float, default=0.5)  # TODO Add in thresholds for annotate_sex
 
     args = parser.parse_args()
 
