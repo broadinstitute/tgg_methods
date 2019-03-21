3ea861cc6fc39f3cdbd5d92736f8b58c5a904ccd
Mike Wilson <mwilson@broadinstitute.org>
2019-03-21 10:23:47 -0400

    Originall pushed dated script

--- retrieve_ped_from_seqr.py
+++ retrieve_ped_from_seqr.py
@@ -1,38 +1,65 @@
 import getpass
-import json
 import requests
-from pprint import pprint
+import argparse
 
-SEQR_URL = "https://seqr.broadinstitute.org"
-LOGIN_URL = SEQR_URL+"/login"
-INDIVIDUAL_API_URL = SEQR_URL+"/api/project/<project_guid>/export_project_individuals?file_format=json"
 
-seqr_username = "mwilson@broadinstitute.org"
-seqr_password = getpass.getpass()     # prompt for password
+def pull_project_peds(args):
+    '''
+    Uses the seqr API to retrieve a list of projects' pedigrees. Writes a single pedigree,
+    'compiled_pedigree.txt', using the retrieved pedigrees.
+    :param args: List of projects to be retrieved
+    '''
+    projects = args.projects
+    login_url = "https://seqr.broadinstitute.org/login"
+    individual_api_url = "https://seqr.broadinstitute.org/api/project/{project_guid}/export_project_individuals?file_format=tsv"
 
-s = requests.Session()
-response = s.get(LOGIN_URL)    # get CSRF cookies
-response.raise_for_status()
+    seqr_username = input("What is your seqr username? ")
+    seqr_password = getpass.getpass()
 
-csrf_token = s.cookies.get('csrftoken')
-response = s.post(
-  LOGIN_URL,
-  data={
-    'username_or_email': seqr_username,
-    'password': seqr_password,
-    'csrfmiddlewaretoken': csrf_token
-  },
-  headers={
-    'Content-Type': 'application/x-www-form-urlencoded'
-  })    # submit login
+    s = requests.Session()
+    response = s.get(login_url)    # get CSRF cookies
+    response.raise_for_status()
 
-response.raise_for_status()
+    csrf_token = s.cookies.get('csrftoken')
+    response = s.post(
+        login_url,
+        data={
+            'username_or_email': seqr_username,
+            'password': seqr_password,
+            'csrfmiddlewaretoken': csrf_token
+        },
+        headers={
+            'Content-Type': 'application/x-www-form-urlencoded'
+        })    # submit login
+    response.raise_for_status()
+    temp_ped = open('temp_pedigree.txt', 'w')
+    errors = open('projects_not_retrieved.txt', 'w')
 
-project_guid = input("Enter seqr project guid (eg. R0390_1000_genomes_demo): ")
+    with open(projects, 'r') as project_list:
+        for project_guid in project_list:
+            project_guid = project_guid.rstrip()
+            response = s.get(individual_api_url.format(**locals()))   # get tsv from API
+            if response.status_code == requests.codes.ok:
+                temp_ped.write(response.text)
+            else:
+                errors.write('{project_guid}\n'.format(**locals()))
 
-response = s.get(INDIVIDUAL_API_URL.format(**locals()))   # get json from API
-response.raise_for_status()
-pprint(response.text)
-# parse json response
-#json_data = [json.loads(row) for row in response.content.strip().split("\n")]
-#pprint(json_data)
+    errors.close()
+    temp_ped.close()
+
+    final_ped = open('compiled_pedigree.ped', 'w')
+
+    with open('temp_pedigree.txt', 'r') as ped:
+        final_ped.write('Family_ID\tIndividual_ID\tPaternal_ID\tMaternal_ID\tSex\tAffected_Status\tNotes\n')
+        for line in ped:
+            if line != "Family ID\tIndividual ID\tPaternal ID\tMaternal ID\tSex\tAffected Status\tNotes\n":
+                final_ped.write(line)
+
+    final_ped.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--projects', help="File of projects in to retrieve, separated by newline", required=True)
+    args = parser.parse_args()
+    pull_project_peds(args)
