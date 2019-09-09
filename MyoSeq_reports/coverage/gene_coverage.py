8ca4ca470224349d65dd183b856d6a38335b4cb9
Katherine Chao <kchao@broadinstitute.org>
2019-09-09 13:20:56 -0400

    Removed coverage threshold counts from gene_coverage.py (doesn't look like Monkol uses them)

--- MyoSeq_reports/coverage/gene_coverage.py
+++ MyoSeq_reports/coverage/gene_coverage.py
@@ -11,8 +11,6 @@ out = os.environ['OUT']
 # get gene name
 gene = basename(tsv).split('.')[0]
 
-# set coverage thresholds
-thresholds = [1, 5, 10, 15, 20, 25, 30, 50, 100]
 with gzip.open(tsv) as t, open(out, 'w') as o:
 
     # write header
@@ -23,15 +21,6 @@ with gzip.open(tsv) as t, open(out, 'w') as o:
         pos = line[1]
         n_samples = len(line)
         total_cov = 0
-        threshold_counts = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-        # check if sample's coverage exceeds thresholds; if so, increment threshold_counts
-        for i in xrange(2, n_samples):
-            samp_cov = int(line[i])
-            total_cov += samp_cov
-            for j in xrange(len(thresholds)):
-                if samp_cov >= thresholds[j]:
-                    threshold_counts[j] += 1
 
         # get mean and median cov
         mean_cov = float(total_cov) / (n_samples - 2)
@@ -41,8 +30,5 @@ with gzip.open(tsv) as t, open(out, 'w') as o:
             median_cov = sorted_cov[n_samples/2]
         else:
             median_cov = (sorted_cov[(n_samples/2) - 1] + sorted_cov[(n_samples/2)]) / 2
-        for i in xrange(len(threshold_counts)):
-            cov = threshold_counts[i]
-            threshold_counts[i] = round(float(cov) / len(sorted_cov), 2)
 
-        o.write('{}\t{}\t{}\t{}\t{}\t{}\n'.format(gene, chrom, pos, mean_cov, median_cov, '\t'.join(threshold_counts))
+        o.write('{}\t{}\t{}\t{}\t{}\n'.format(gene, chrom, pos, mean_cov, median_cov))
