19d682797109d6f7123923e8fcfb38d79af43e50
Katherine Chao <kchao@broadinstitute.org>
2019-09-09 14:29:22 -0400

    Updated function docstrings for ancestry and sexcheck scripts

--- MyoSeq_reports/coverage/sexcheck.py
+++ MyoSeq_reports/coverage/sexcheck.py
@@ -6,8 +6,14 @@ import logging
 
 
 def ped_sex(ped, samples):
-    '''Gets reported sex for all samples'''
+    """
+    Gets reported sex for all samples from pedigree file
 
+    :param str ped: Name of pedigree file
+    :param list samples: List of requested sample IDs
+    :return: Dictionary; key: sample, value: reported sex
+    :rtype: dict
+    """
     reported = {}
     with open(ped) as p:
         for line in p:
@@ -17,14 +23,22 @@ def ped_sex(ped, samples):
                 reported[sid] = sex
 
     # check if any samples are missing and print missing samples to stdout
-    ancestry.check_missing(samples, reported.keys(), "ped")
+    ancestry.check_missing(samples, reported.keys(), 'ped')
 
     return reported
 
 
 def compare(infer, samples, reported, out):
-    '''Gets inferred sex for all samples, compares to reported sex, and writes to output'''
+    """
+    Gets inferred sex for all samples, compares to reported sex, and writes to output
 
+    :param str infer: Name of file with inferred sex
+    :param list samples: List of requested sample IDs
+    :param dict reported: Dictionary of sample IDs and reported sex
+    :param str out: Name of output TSV
+    :return: None
+    :rtype: None
+    """
     # get inferred sex for all samples of interest
     # s is_female   f_stat  n_called    expected_homs   observed_homs   sex y_cov   twenty_cov  normalized_y_coverage
     with open(infer) as i, open(out, 'w') as o:
@@ -38,37 +52,37 @@ def compare(infer, samples, reported, out):
 
                 # check for conflicts between reported and inferred sex and write to output
                 if inferred == reported[s]:
-                    o.write("{}\t{}\t{}\t{}\t{}\n".format(s, reported[s], inferred, f_stat, "CONCORD"))
+                    o.write('{}\t{}\t{}\t{}\t{}\n'.format(s, reported[s], inferred, f_stat, 'CONCORD'))
                 else:
-                    o.write("{}\t{}\t{}\t{}\t{}\n".format(s, reported[s], inferred, f_stat, "CONFLICT"))
+                    o.write('{}\t{}\t{}\t{}\t{}\n'.format(s, reported[s], inferred, f_stat, 'CONFLICT'))
 
     # check if any samples are missing from inferred file
-    ancestry.check_missing(samples, found, "inferred sex")
+    ancestry.check_missing(samples, found, 'inferred sex')
                 
 
 def main(args):
 
-    logging.info("Getting samples from file")
+    logging.info('Getting samples from file')
     samples = ancestry.get_samp(args.samp)
 
-    logging.info("Getting reported sex for each sample")
+    logging.info('Getting reported sex for each sample')
     reported = ped_sex(args.ped, samples)
 
-    logging.info("Comparing inferred and reported sex and writing to output")
+    logging.info('Comparing inferred and reported sex and writing to output')
     compare(args.infer, samples, reported, args.out)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     
      # Set up logger
     logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
     # Get args from command line
-    parser = argparse.ArgumentParser(description="Checks inferred sex against reported sex for MYOSEQ reports")
-    parser.add_argument("-p", "--ped", help="ped file", required=True)
-    parser.add_argument("-s", "--samp", help="input list of samples", required=True)
-    parser.add_argument("-i", "--infer", help="input list of inferred sex", required=True)
-    parser.add_argument("-o", "--out", help="output TSV", required=True)
+    parser = argparse.ArgumentParser(description='Checks inferred sex against reported sex for MYOSEQ reports')
+    parser.add_argument('-p', '--ped', help='ped file', required=True)
+    parser.add_argument('-s', '--samp', help='input list of samples', required=True)
+    parser.add_argument('-i', '--infer', help='input list of inferred sex', required=True)
+    parser.add_argument('-o', '--out', help='output TSV', required=True)
     args = parser.parse_args()
 
     main(args)
