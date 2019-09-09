c019b90bac2f2f3ae2d02453e807992866cb87b5
Katherine Chao <kchao@broadinstitute.org>
2019-09-09 13:59:32 -0400

    Initial commit of script to parse sample ancestry

---
+++ MyoSeq_reports/coverage/ancestry.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import argparse
+import logging
+
+
+def check_missing(samples, check, fname):
+    """Checks for samples missing from file"""
+
+    missing = set(samples) - set(check)
+    if len(missing) > 0:
+        logging.warning("{} not found in {} file".format(",".join(missing), fname))
+
+
+def get_samp(samp):
+    """Get samples from input file (one sample per line)"""
+
+    samples = []
+    with open(samp) as s:
+        for line in s:
+            samples.append(line.strip())
+    return samples
+
+
+def parse_pca(pca, samples, out):
+    """Get inferred ancestry from PCA file and write to output TSV"""
+
+    # create dict to map pca ancestry to ancestry text for report
+    # s PROJECT DATA_TYPE   RESEARCH_PROJECT_NUMBER PRODUCT INDIVIDUAL_NAME ANALYSIS_END    PCT_CONTAMINATION   AL_PCT_CHIMERAS WGS_MEAN_COVERAGE   HS_MEAN_TARGET_COVERAGE HS_PCT_TARGET_BASES_20X MYRANK  seqr_id filtered_callrate   filter_flags    qc_platform qc_popprob_afr  prob_amr    prob_asj    prob_eas    prob_est_b1 prob_est_b2 prob_fin    prob_nfe    prob_oth    prob_sas    pop_PC1 pop_PC2 pop_PC3 pop_PC4 pop_PC5 pop_PC6 data_type   known_pop   source  fail_n_snp  fail_r_ti_tv    fail_r_insertion_deletion   fail_n_insertion    fail_n_deletion fail_r_het_hom_var  pop_platform_filters    sample_qc.dp_stats.mean sample_qc.dp_stats.stdev    sample_qc.dp_stats.min  sample_qc.dp_stats.max  sample_qc.gq_stats.mean sample_qc.gq_stats.stdev    sample_qc.gq_stats.minsample_qc.gq_stats.max    sample_qc.call_rate sample_qc.n_called  sample_qc.n_not_called  sample_qc.n_filtered    sample_qc.n_hom_ref sample_qc.n_het sample_qc.n_hom_var sample_qc.n_non_ref sample_qc.n_singleton   sample_qc.n_snp sample_qc.n_insertion   sample_qc.n_deletion    sample_qc.n_transition  sample_qc.n_transversion    sample_qc.n_star    sample_qc.r_ti_tv   sample_qc.r_het_hom_var sample_qc.r_insertion_deletion  sample_qc.f_inbreeding.f_stat   sample_qc.f_inbreeding.n_called sample_qc.f_inbreeding.expected_homs    sample_qc.f_inbreeding.observed_homs
+    popmap = {"amr": "Admixed American", "asj": "Ashkenazi Jewish", "eas": "East Asian", "fin": "Finnish", "nfe": "European", "oth": "Other", "sas": "Southeast Asian", "afr": "African"}
+
+    with open(pca) as p, open(out, 'w') as o:
+
+        # pull indices for relevant columns from header
+        header = p.readline().strip().split('\t')
+        sidx = header.index("s")
+        pidx = header.index("qc_pop")
+        
+        # get inferred ancestry for desired samples and write to output
+        found = []
+        for line in p:
+            line = line.strip().split('\t')
+            s = line[sidx]
+            pop = line[pidx]
+            if s in samples:
+                found.append(s)
+                print s, pop
+                o.write("{}\t{}\n".format(s, popmap[pop]))
+
+    # check if any samples are missing and print missing samples to stdout
+    check_missing(samples, found, "pca")
+    
+
+def main(args):
+
+    logging.info("Getting samples from input file")
+    samples = get_samp(args.samp)
+
+    logging.info("Getting inferred ancestry from PCA and writing to output")
+    parse_pca(args.pca, samples, args.out)
+
+
+if __name__ == "__main__":
+    
+     # Set up logger
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+    # Get args from command line
+    parser = argparse.ArgumentParser(description="Pulls ancestry from PCA file for MYOSEQ reports")
+    parser.add_argument("-p", "--pca", help="input TSV (PCA)", required=True)
+    parser.add_argument("-s", "--samp", help="input list of samples", required=True)
+    parser.add_argument("-o", "--out", help="output TSV", required=True)
+    args = parser.parse_args()
+
+    main(args)
