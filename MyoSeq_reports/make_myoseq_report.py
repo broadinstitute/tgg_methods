41956ba6192d76fc60b4965cca80fa5e148ebe98
Katherine Chao <kchao@broadinstitute.org>
2019-09-23 15:44:20 -0400

    Added functions to use cat and awk to make_myoseq_report.py

--- MyoSeq_reports/make_myoseq_report.py
+++ MyoSeq_reports/make_myoseq_report.py
@@ -30,14 +30,41 @@ def awk(sample: str, fname: str) -> str:
     return line
 
 
-def get_patient_details(proband: str, dirname: str) -> str:
+def cat(fname: str, outname: str) -> None:
+    '''
+    cats one file into output file.
+
+    :param fname: File name with information to be cat
+    :param str outname: Output file name
+    :return: None
+    :rtype: None
+    '''
+    cmd = '''cat {} >> {}'''.format(fname, outname)
+    Popen([cmd], shell=True).communicate()
+
+
+def append_out(info: str, outname: str) -> None:
+    '''
+    Opens output file and appends information
+
+    :param str info: Information to be added into file
+    :param str outname: Output file name
+    :return: None
+    :rtype: None
+    '''
+    with open(outname, 'a') as o:
+        o.write(info)
+
+
+def get_patient_details(proband: str, dirname: str, outname: str) -> None:
     '''
     Formats proband ID, sex, and ancestry for first page of report
 
     :param str proband: Proband ID 
-    :param str dirname: Name of top level directory with all of sample information for reports
-    :return: Formatted string to print out to .tex file
-    :rtype: str
+    :param str dirname: Name of top level directory with all sample information for reports
+    :param str outname: Name of output file
+    :return: None
+    :rtype: None
     '''
 
     proband_id = proband.replace('_', '')
@@ -53,26 +80,58 @@ def get_patient_details(proband: str, dirname: str) -> str:
     sample,ancestry = ancestry_line.strip().split('\t')
     patient_details += '{\\large \\textbf{Inferred Ancestry (Reported):} ' + f'{ancestry} (European)' + '}' + f'\n{LINE_BREAK} {LINE_BREAK} {LINE_BREAK} \n' 
 
-    return patient_details
+    append_out(patient_details, outname)
+
+
+def get_report_variants(proband: str, dirname: str, unsolved: bool, outname: str, resources: str) -> None:
+    '''
+    Formats any candidate variants (tagged 'REPORT' in seqr) for first page of report
+
+    :param str proband: Proband ID
+    :param str dirname: Name of top level directory with all sample information for reports
+    :param bool unsolved: Whether patient is unsolved
+    :param str outname: Output file name
+    :param str resources: Directory with tex files
+    :return: None
+    :rtype: None
+    '''
+    if unsolved:
+        cat(f'{resources}/myoseq_template_no_candidate_variants_notes.tex', outname)
+    else:
+        report_file = f'{dirname}/seqr/variants/{proband}.flagged.txt' 
+
+    append_out(report_str, outname)
 
 
 def main(args):
 
-    dirname = args.dirname
     proband = args.proband
+    dirname = args.dirname
+    resources = args.resources
+    unsolved = args.unsolved
+    outname = f'{args.out}/{proband}.tex'
 
-    # create string to be written to output tex file
-    out_string = ''
+    logger.info('Setting up the report header')
+    cat(f'{resources}/myoseq_template_header.tex', outname)
+
     logger.info('Preparing top of first page of reports (ID, sex, ancestry)')
-    out_string += get_patient_details(proband, dirname)
+    get_patient_details(proband, dirname, outname)
+
+    logger.info('Finishing first page of reports (REPORT genes box)')
+    get_report_variants(proband, dirname, unsolved, outname, resources)
+
+    logger.info('Preparing appendix of all variants in gene list')
+    cat(f'{resources}/myoseq_template_appendix_gene_list.tex', outname)
 
 
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='Creates patient section of tex file for pdflatex')
-    parser.add_argument('-c', '--cnv', help='Samples have CNV candidates', action='store_true', default=False)
-    parser.add_argument('-s', '--sma', help='Samples are SMA carriers', action='store_true', default=False)
+    parser.add_argument('-c', '--cnv', help='Samples have CNV candidates', action='store_true')
+    parser.add_argument('-s', '--sma', help='Samples are SMA carriers', action='store_true')
+    parser.add_argument('-u', '--unsolved', help='Sample is unsolved (no candidates)', action='store_false')
     parser.add_argument('-d', '--dirname', help='Top level directory that contains all data for reports', required=True)
+    parser.add_argument('-r', '--resources', help='Directory containing tex files', required=True)
     parser.add_argument('-p', '--proband', help='Proband ID', required=True)
     parser.add_argument('-o', '--out', help='Output directory', required=True)
     args = parser.parse_args()
