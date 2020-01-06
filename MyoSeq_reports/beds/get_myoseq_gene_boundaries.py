40f08e26bb1f5c83aeefb0d323e938a8c2dd93d7
Katherine Chao <kchao@broadinstitute.org>
2020-01-06 11:56:43 -0500

    Added newline write when writing beds to get_myoseq_gene_boundaries.py

--- MyoSeq_reports/beds/get_myoseq_gene_boundaries.py
+++ MyoSeq_reports/beds/get_myoseq_gene_boundaries.py
@@ -72,7 +72,7 @@ def write_beds(gene_boundaries, out) -> None:
     for gene in gene_boundaries:
         out_bed = f'{out}/{gene}.bed'
         with open(out_bed, 'w') as o:
-            o.write(f'{gene_boundaries[gene][0]}\t{gene_boundaries[gene][1]}\t{gene_boundaries[gene][2]}')
+            o.write(f'{gene_boundaries[gene][0]}\t{gene_boundaries[gene][1]}\t{gene_boundaries[gene][2]}\n')
 
 
 def main(args):
