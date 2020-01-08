849fdbb817e722b1fca82449b21507557ec9aecf
Katherine Chao <kchao@broadinstitute.org>
2020-01-08 15:24:08 -0500

    Fixed typos in bigquery code

--- MyoSeq_reports/seqr/bigquery.py
+++ MyoSeq_reports/seqr/bigquery.py
@@ -6,6 +6,7 @@ import logging
 import os
 from utils import get_samples
 
+
 logging.basicConfig(format='%(asctime)s (%(name)s %(lineno)s): %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 logger = logging.getLogger('bigquery')
 logger.setLevel(logging.INFO)
@@ -24,27 +25,27 @@ def parse_var(var, samples):
     with open(var) as v:
 
         # get header of seqr export file
-        header = v.readline().strip().split(',')
+        header = v.readline().strip().split('\t')
         # this is to double check that seqr download format has not changed
-        logger.info('seqr format check: {}, {}'.format(header[0:6], header[19:24]))
+        logger.info('seqr format check: {}, {}'.format(header[0:6], header[19:23]))
         
         for line in v:
-            line = line.strip().split(',')
+            line = line.strip().split('\t')
             
             # extract necessary information for variant
             # chrom, pos, ref, alt, gene, worst_consequence, rsid, hgvs, hgvps, clinvar, gold stars
-            variant_info = line[0:6] + line[19:24]
+            variant_info = line[0:6] + line[19:23]
             variant = '{}-{}-{}-{}'.format(variant_info[0], variant_info[1], variant_info[2], variant_info[3])
 
             # every field at index 25 and after should have samle level information
-            sample_info = line[25:]
+            sample_info = line[27:]
 
-            if variant not in variants:
-                variants[variant] = []
             for sample in sample_info:
                 if ':' in sample:
                     sample = sample.split(':')
                     if sample[0] in samples:
+                        if variant not in variants:
+                            variants[variant] = []
                         variants[variant].append(sample[0])
     return variants
 
@@ -72,8 +73,8 @@ def write_bigquery_tsv(variants, out):
 
 def main(args):
 
-    logger.info('Getting individual IDs from ped file')
-    samples = get_samples(args.ped)
+    logger.info('Getting individual IDs from sample list')
+    samples = get_samples(args.samp)
 
     logger.info('Getting variants from seqr')
     variants = parse_var(args.var, samples)
@@ -87,7 +88,7 @@ if __name__ == '__main__':
     # Get args from command line
     parser = argparse.ArgumentParser(description='Reformats seqr variant export for biqquery AF lookup')
     parser.add_argument('-v', '--var', help='input (seqr variants)', required=True)
-    parser.add_argument('-p', '--ped', help='input ped', required=True)
+    parser.add_argument('-s', '--samp', help='input sample list', required=True)
     parser.add_argument('-o', '--out', help='output TSV', required=True)
     args = parser.parse_args()
 
