2d204e9102ccc47eef1eaa15eed7df417643cc43
Mike Wilson <mwilson@broadinstitute.org>
2020-06-22 13:11:20 -0400

    Added documentation

--- compare_md5.py
+++ compare_md5.py
@@ -15,6 +15,12 @@ logger.setLevel(logging.INFO)
 
 
 def compare_md5_dict(origin_md5_dict, dest_md5_dict):
+    """Compare the md5 origin and destination dictionaries. If md5s do not match,
+    document in dictionary. If file is missing, document in set of missing files.
+    :param origin_md5_dict: dictionary of paths and md5 values for origin files
+    :param dest_md5_dict: dictionary of paths and md5 values for destination files
+    :return dictionary of filesets and unequal md5 value, set of missing files
+    """
     needs_attention = {}
     files_missing = set([])
     logger.info('In comparison module')
@@ -28,6 +34,12 @@ def compare_md5_dict(origin_md5_dict, dest_md5_dict):
 
 
 def create_md5_dict(file_dict)->dict:
+    """Create a dictionary for each subfile in a directory with file key 
+    being directory + subfile path and values being the full path and md5
+    containing md5s.
+    :param file_dict: dictionary of directories 
+    :return dictionary of files, paths, and md5 value
+    """
     md5_dict = {}
     for key in file_dict:
         logger.info(f'Examining the directory: {key}')
@@ -39,31 +51,41 @@ def create_md5_dict(file_dict)->dict:
 
 
 def read_hail_paths(bucket, bucket_file):
+    """Read file from google bucket and create sets of origin files 
+    and destination files
+    :param bucket: top level google bucket with project e.g. "gnomad-public"
+    :param bucket_file: Path to file containing columns of origin and destination files
+    :return set of origin files, set of destination files
+    """
     bucket.get_blob(bucket_file).download_to_filename('paths')
-
+    origin = []
+    dest = []
     with open('paths', 'r') as paths:
         csv_input = csv.reader(paths, delimiter='\t', skipinitialspace=True)
-        origin = []
-        dest = []
         for rows in csv_input:
             origin.append(rows[0])
             dest.append(rows[1])
     return origin, dest
 
 
-def create_file_dict(file_list):
+def create_file_dict(directory_list):
+    """Generate a dictionary where top level directories are keys and the set of
+    all files in each top level directory are the values
+    :param directory_list: List of top level directories
+    :return file_dict: dictionary of directories and their subfiles
+    """
     file_dict = {}
-    for file in file_list:
-        logger.info(f'Examining {file}')
+    for directory in directory_list:
+        logger.info(f'Building dictionary for {directory}')
        # Remove file string from file name and then convert "/" to "_", no need for new key assignm
         subfiles = (
-                subprocess.check_output(["gsutil", "ls", "-r", file])
+                subprocess.check_output(["gsutil", "ls", "-r", directory])
                 .decode("utf8")
                 .strip()
                 .split("\n")
             )
         subfiles = [f for f in subfiles if not (f.endswith("/") or f.endswith(":") or f == '')]
-        file_dict[file] = subfiles
+        file_dict[directory] = subfiles
     return file_dict
 
 
@@ -99,7 +121,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input-bucket-file", help="File with two columns: origin and destination", required=True)
-    parser.add_argument("--output-bucket-file", help="File for comparison results", required=True)
+    parser.add_argument("--output-bucket-file", help="File to output comparison results", required=True)
     parser.add_argument("--bucket", help="Bucket where input and ouptut files live", required=True)
 
     args = parser.parse_args()
