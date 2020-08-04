7224de15daf1f8d620b0244b6f7840447fdd2594
Katherine Chao <kchao@broadinstitute.org>
2020-08-04 10:47:03 -0400

    Removed unnecessary arg

--- utils/subset.py
+++ utils/subset.py
@@ -1,7 +1,7 @@
 import argparse
 import logging
 import hail as hl
-from gnomad.utils.generic import subset_samples_and_variants
+from gnomad.utils.filtering import subset_samples_and_variants
 
 
 logging.basicConfig(
@@ -10,6 +10,34 @@ logging.basicConfig(
 )
 logger = logging.getLogger("subset")
 logger.setLevel(logging.INFO)
+
+
+def remap_sample_ids(mt, remap_path):
+    """
+    Remap the MatrixTable's sample ID, 's', field to the sample ID used within seqr, 'seqr_id'
+    If the sample 's' does not have a 'seqr_id' in the remap file, 's' becomes 'seqr_id'
+    :param mt: MatrixTable from VCF
+    :param remap_path: Path to a file with two columns 's' and 'seqr_id'
+    :return: MatrixTable remapped and keyed to use seqr_id
+    """
+    remap_ht = hl.import_table(remap_path, key='s')
+    missing_samples = remap_ht.anti_join(mt.cols()).collect()
+    remap_count = remap_ht.count()
+
+    if len(missing_samples) != 0:
+        logger.error(
+            f'Only {remap_ht.semi_join(mt.cols()).count()} out of {remap_count} '
+            'remap IDs matched IDs in the variant callset.\n'
+            f'IDs that aren\'t in the callset: {missing_samples}\n'
+            f'All callset sample IDs:{mt.s.collect()}', missing_samples
+        )
+
+    mt = mt.annotate_cols(**remap_ht[mt.s])
+    remap_expr = hl.cond(hl.is_missing(mt.seqr_id), mt.s, mt.seqr_id)
+    mt = mt.annotate_cols(seqr_id=remap_expr, vcf_id=mt.s)
+    mt = mt.key_cols_by(s=mt.seqr_id)
+    logger.info(f'Remapped {remap_count} sample ids...')
+    return mt
 
 
 def main(args):
@@ -28,6 +56,12 @@ def main(args):
         mt = hl.read_matrix_table(args.mt_path)
 
     logger.info(f"Input MT counts: {mt.count()}")
+
+    if args.mapping:
+        logger.info("Mapping sample IDs")
+        logger.warning("Assuming mapping file has header!")
+        mt = remap_sample_ids(mt, args.mapping)       
+ 
     logger.info("Subsetting to specified samples and their variants...")
     mt = subset_samples_and_variants(mt, args.samp, header=args.header, table_key=args.table_key)
     logger.info(f"Subset MT counts: {mt.count()}")
@@ -55,6 +89,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-s", "--samp", help="Path to file with list of samples to subset"
     )
+    parser.add_argument("--mapping", help="Path to file with sample ID mapping")
     parser.add_argument(
         "--header", help="Whether sample list file has a header. Specify only if False", action="store_false"
     )
