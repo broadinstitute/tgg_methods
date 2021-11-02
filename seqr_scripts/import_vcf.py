5e16d106ff84c091a5f78f4ec88564ae6f86ffa0
wphu <17106856+williamphu@users.noreply.github.com>
2021-11-02 14:16:53 -0400

    update based on PR comments

--- seqr_scripts/import_vcf.py
+++ seqr_scripts/import_vcf.py
@@ -17,7 +17,7 @@ logger.setLevel(logging.INFO)
 def main(args):
 
     hl.init(
-        default_reference="GRCh38",
+        default_reference=args.import_build,
         log="/import_vcf.log",
         tmp_dir="hdfs:///import_vcf.tmp/",
     )
@@ -25,7 +25,7 @@ def main(args):
     logger.info("Importing filters info...")
     filters_ht = hl.import_vcf(
         args.part_two_path,
-        reference_genome="GRCh38",
+        reference_genome=args.import_build,
         force_bgz=True,
         find_replace=("nul", "."),
     ).rows()
@@ -47,17 +47,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         "This script imports/merges callset filter and genotype information into one MatrixTable and exports to VCF. Currently required only for internal exomes callsets."
     )
+    parser.add_argument("--part_one_path", help="Path to part one outputs from callset VCFs. This bucket has a nested structure and contains one bucket per chromosome. E.g., gs://fc-2b089793-51e4-46f9-9ed0-c317f8d547eb/RDG_Broad_WES_Internal_v3/part_one_outputs/chr*/RDG_Broad_WES_Internal_chr*.*.hard_filtered_with_genotypes.vcf.gz.")
     parser.add_argument(
         "--part_two_path",
-        help="Path to part one outputs from callset (contain filter information)",
+        help="Path to part two outputs from callset (VCF shards containing filter information). E.g., gs://fc-2b089793-51e4-46f9-9ed0-c317f8d547eb/RDG_Broad_WES_Internal_v3/part_two_outputs/RDG_Broad_WES_Internal.filtered.*.vcf.gz."
     )
-    parser.add_argument("--part_one_path", help="Path to input VCFs")
     parser.add_argument(
         "--import_build",
         help="Reference build to use when importing VCF",
         default="GRCh38",
     )
-    parser.add_argument("--vcf_out", help="Output path for VCF")
+    parser.add_argument("--vcf_out", help="Output path for VCF. E.g., gs://seqr-datasets/v02/GRCh38/RDG_WES_Broad_Internal/v12/RDG_WES_Broad_Internal.vcf.bgz")
     args = parser.parse_args()
 
     main(args)
