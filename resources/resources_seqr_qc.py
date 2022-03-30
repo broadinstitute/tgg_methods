94974c9292356bdc3fd954cd23493559ec70e7bf
Mike Wilson <mwilson@broadinstitute.org>
2022-03-30 08:11:41 -0400

    Move temp files to temp bucket

--- resources/resources_seqr_qc.py
+++ resources/resources_seqr_qc.py
@@ -3,7 +3,7 @@ class VCFDataTypeError(Exception):
         super().__init__(message)
 
 
-def sample_qc_folder(
+def temp_sample_qc_folder(
     build: int, data_type: str, data_source: str, version: int, is_test=False
 ) -> str:
     """
@@ -16,9 +16,23 @@ def sample_qc_folder(
     :return:
     """
     if is_test:
-        return f"gs://seqr-datasets/v02/GRCh{build}/RDG_{data_type}_Broad_{data_source}/v{version}/sample_qc/test"  # TODO revisit where test should be - want to use same resource folder to avoid copying over agian
+        return f"gs://seqr-temp/v02/GRCh{build}/RDG_{data_type}_Broad_{data_source}/v{version}/sample_qc/test"  # TODO revisit where test should be - want to use same resource folder to avoid copying over agian
     else:
-        return f"gs://seqr-datasets/v02/GRCh{build}/RDG_{data_type}_Broad_{data_source}/v{version}/sample_qc"
+        return f"gs://seqr-temp/v02/GRCh{build}/RDG_{data_type}_Broad_{data_source}/v{version}/sample_qc"
+
+def final_sample_qc_folder(
+    build: int, data_type: str, data_source: str, version: int, is_test=False
+) -> str:
+    """
+    Returns string with file path to callset version
+    :param build: The genome build: "38" or "37"
+    :param data_type: String of WES or WGS
+    :param data_source: Data is External or Internal
+    :param version: int version of callset
+    :param is_test: Used if running pipeline on designated test data
+    :return:
+    """
+    return f"gs://seqr-datasets/v02/GRCh{build}/RDG_{data_type}_Broad_{data_source}/v{version}/sample_qc"
 
 
 def callset_vcf_path(
@@ -42,28 +56,21 @@ def mt_path(
     Returns MatrixTable for seqr sample qc purposes: can be exomes or genomes, internal or external data.
     Generated from vcf.
     """
-    return f"{sample_qc_folder(build, data_type, data_source, version, is_test)}/resources/callset.mt"
+    return f"{temp_sample_qc_folder(build, data_type, data_source, version, is_test)}/resources/callset.mt"
 
 
 def seq_metrics_path(build: int, data_type: str, data_source: str, version: int) -> str:
     """
     Path to metadata file associated with samples in the callset for seqr sample QC.
     """
-    return f"{sample_qc_folder(build, data_type, data_source, version)}/resources/callset_seq_metrics.tsv"
+    return f"{final_sample_qc_folder(build, data_type, data_source, version)}/resources/callset_seq_metrics.tsv"
 
 
 def remap_path(build: int, data_type: str, data_source: str, version: int) -> str:
     """
     Path to remap ID file which needs to be stored in the callset's bucket prior to launching pipeline
     """
-    return f"{sample_qc_folder(build, data_type, data_source, version)}/resources/remap.tsv"
-
-
-def project_map_path(build: int, data_type: str, data_source: str, version: int) -> str:
-    """
-    Path to project mapping file which needs to be stored in the callset's bucket prior to launching pipeline
-    """
-    return f"{sample_qc_folder(build, data_type, data_source, version)}/resources/project_map.tsv"
+    return f"{final_sample_qc_folder(build, data_type, data_source, version)}/resources/remap.tsv"
 
 
 def missing_metrics_path(
@@ -72,7 +79,7 @@ def missing_metrics_path(
     """
     Path to file containing sample IDs that do not have any seq metrics.
     """
-    return f"{sample_qc_folder(build, data_type, data_source, version)}/missing_metrics/samples_missing_metrics.tsv"
+    return f"{temp_sample_qc_folder(build, data_type, data_source, version)}/missing_metrics/samples_missing_metrics.tsv"
 
 
 def mt_temp_path(
@@ -81,7 +88,7 @@ def mt_temp_path(
     """
     Temporary MatrixTable path that should be deleted once pipeline is complete
     """
-    return f"{sample_qc_folder(build, data_type, data_source, version, is_test)}/temp/mt_temp.mt"
+    return f"{temp_sample_qc_folder(build, data_type, data_source, version, is_test)}/temp/mt_temp.mt"
 
 
 def sample_qc_tsv_path(
@@ -92,7 +99,7 @@ def sample_qc_tsv_path(
     Contains vcf sample ID, seqr sample ID, callrate, chimera, contamination, coverage, pedigree information,
     provided gender and affected status, computed gender, platform, ancestry, inbreeding coefficient.
     """
-    return f"{sample_qc_folder(build, data_type, data_source, version, is_test)}/final_output/seqr_sample_qc.tsv"
+    return f"{final_sample_qc_folder(build, data_type, data_source, version, is_test)}/final_output/seqr_sample_qc.tsv"
 
 
 def sample_qc_ht_path(
@@ -103,7 +110,7 @@ def sample_qc_ht_path(
     Contains vcf sample ID, seqr sample ID, callrate, chimera, contamination, coverage, pedigree information,
     provided gender and affected status, computed gender, platform, ancestry, inbreeding coefficient.
     """
-    return f"{sample_qc_folder(build, data_type, data_source, version, is_test)}/final_output/seqr_sample_qc.ht"
+    return f"{temp_sample_qc_folder(build, data_type, data_source, version, is_test)}/final_output/seqr_sample_qc.ht"
 
 
 def rdg_gnomad_pop_pca_loadings_ht_path(build: int) -> str:
